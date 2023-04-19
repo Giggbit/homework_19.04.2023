3e3c07f9004063fc8e89fca19937014232e7a08f
Giggbit <danya19981017@gmail.com>
2023-04-19 10:39:31 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Многопоточность.Метод_Invoke_homework_19._04._2023
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+            
+        }
+
+        private void search_Click(object sender, EventArgs e) {
+            if (textBox1.Text != null) {
+                
+            }
+        }
+    }
+}
